7f056566bf1bf6f21f927e128efc8641b823ee66
Edson Ricardo <edsonricardo962@gmail.com>
2023-08-03 13:34:16 -0300

    Adicionando minhas tecnologias

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,38 @@
 
+function informacaoConfigPerfil(perfil) {
+    const foto = document.getElementById('profile.photo')
+    foto.src = perfil.photo
+    foto.alt = perfil.name
+
+    const name = document.getElementById('profile.name')
+    name.innerHTML = perfil.name
+
+    const job = document.getElementById('profile.job')
+    job.innerHTML = perfil.job
+
+    const location = document.getElementById('profile.location')
+    location.innerHTML = perfil.location
+
+    const phone = document.getElementById('profile.phone')
+    phone.href = `Telefone: ${perfil.phone}`
+    phone.innerHTML = perfil.phone
+
+    const email = document.getElementById('profile.email')
+    email.innerHTML = perfil.email
+}
+
+function updateSoftSkills(profileData) {
+    const softSkills = document.getElementById('profile.skills.softSkills')
+    softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li> ${skill} </li>`).join('')
+
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+    hardSkillsSkills.innerHTML = profileData.skills.hardSkills.logo.map(img => `<li> ${img} </li>`).join('')
+}
+
 (async () => {
     const perfil = await perfilApi()
+    informacaoConfigPerfil(perfil)
+    updateSoftSkills(perfil)
+
     console.log(perfil)
 })()
